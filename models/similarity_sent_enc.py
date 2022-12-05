a7bc8854a14845eeba871f1cbd7739c959c0cca6
OrionLi545 <haoxuanl@csail.mit.edu>
2022-12-05 18:13:15 -0500

    fixed bugs in semantic similarity

--- models/similarity_sent_enc.py
+++ models/similarity_sent_enc.py
@@ -5,6 +5,8 @@ from sentence_transformers import SentenceTransformer, util
 st_model = SentenceTransformer('all-mpnet-base-v2')
 
 def encode_for_similarities(st_model, device, src_ids, rst_logits):
+    
+    # rst_logits is after softmax in this case, sorry for calling it logits
 
     SEP=st_model.tokenizer.sep_token_id
     CLS=st_model.tokenizer.cls_token_id
@@ -18,7 +20,7 @@ def encode_for_similarities(st_model, device, src_ids, rst_logits):
                 torch.ones(src_ids.shape[0], 1, dtype=torch.long).to(device) * SEP], dim=1)
     
     src_embed = st_model[0].auto_model.embeddings.word_embeddings.weight[src_full].to(device)
-    tgt_embed = nn.Softmax(dim=-1)(rst_logits)@st_model[0].auto_model.embeddings.word_embeddings.weight.to(device)
+    tgt_embed = rst_logits@st_model[0].auto_model.embeddings.word_embeddings.weight.to(device)
     tgt_embed = torch.cat([cls_emb, tgt_embed, sep_emb], dim=1)
     
     src_encode = st_model[0].auto_model.forward(inputs_embeds=src_embed)[1]
